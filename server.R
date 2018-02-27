246e01c3d55eace6d5a37a5cad6d1764ba0f8db3
aarya22 <aman.arya524@gmail.com>
2018-02-27 15:53:18 -0800

    Initial commit with highlighted maps and toolbar

---
+++ server.R
@@ -0,0 +1,46 @@
+library("leaflet")
+library("shiny")
+library("dplyr")
+library("geojsonio")
+
+server <- function(input, output) {
+  
+  world <- geojsonio::geojson_read("json/countries.geo.json", what = "sp")
+  
+  # Examples to demonstrate lines
+  mydf <- data.frame(Observation = c("A", "B"),
+                     InitialLat = c(62.469722,48.0975),
+                     InitialLong = c(6.187194, 16.3108),
+                     NewLat = c(51.4749, 51.4882),
+                     NewLong = c(-0.221619, -0.302621),
+                     stringsAsFactors = FALSE)
+  
+  mydf2 <- data.frame(group = c("A", "B"),
+                      lat = c(mydf$InitialLat, mydf$NewLat),
+                      long = c(mydf$InitialLong, mydf$NewLong))
+  
+  # Create the map
+  output$map <- renderLeaflet({
+    leaflet(world) %>%
+      addTiles(
+        urlTemplate = "https://api.mapbox.com/styles/v1/aarya22/cje65iy6k03db2rs272v1h8ld/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWFyeWEyMiIsImEiOiJjamU2NWNvNWUwMDVrMzNvY3Q0NGluZ3o0In0.v3dVCyNNhVq-vFtL237dGw",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = -73.33251, lat = -7.324879e+00, zoom = 3) %>%
+      addPolylines(data = mydf2, lng = ~long, lat = ~lat, group = ~group) %>%
+      addPolygons(
+         weight = 1,
+         opacity = 0.7,
+         dashArray = "3",
+         color = "#d6d6d6",
+         fillOpacity = 0.3,
+         highlight = highlightOptions(
+           weight = 5,
+           color = "#666",
+           dashArray = "",
+           fillOpacity = 0.7,
+           bringToFront = TRUE)
+      )
+  })
+  
+}
---
+++ ui.R
@@ -0,0 +1,40 @@
+library("leaflet")
+library("shiny")
+
+vars <- c(
+  "Is SuperZIP?" = "superzip",
+  "Centile score" = "centile",
+  "College education" = "college",
+  "Median income" = "income",
+  "Population" = "adultpop"
+)
+
+navbarPage("Refugee-Tracker", id="nav",
+           
+  tabPanel("Interactive map",
+    div(class="outer",
+        
+      tags$head(
+          # Include our custom CSS
+          includeCSS("styles.css")
+      ),
+                
+      # If not using custom CSS, set height of leafletOutput to a number instead of percent
+      leafletOutput("map", width = "100%", height = "100%"),
+      
+      # Shiny versions prior to 0.11 should use class = "modal" instead.
+      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                    draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                    width = 330, height = "auto",
+                    
+                    h2("ZIP explorer"),
+                    
+                    selectInput("color", "Color", vars),
+                    selectInput("size", "Size", vars, selected = "adultpop")
+                    
+                    # plotOutput("histCentile", height = 200),
+                    # plotOutput("scatterCollegeIncome", height = 250)
+      )
+    )
+  )
+)
